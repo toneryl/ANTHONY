9ee6a06255f203aaa0cc8a7a2c9a13fa90818a79
toneryl <toneryl@email>
2024-11-07 20:50:05 +0000

    Committing ANTHONY

--- tml-airflow/dags/tml-solutions/NEW-381a/tml_read_MQTT_step_3_kafka_producetotopic_dag-NEW-381a.py
+++ tml-airflow/dags/tml-solutions/NEW-381a/tml_read_MQTT_step_3_kafka_producetotopic_dag-NEW-381a.py
@@ -31,9 +31,9 @@ default_args = {
   'topics' : 'cisco-network-mainstream', # *************** This is one of the topic you created in SYSTEM STEP 2
   'identifier' : 'TML solution',  
   'mqtt_broker' : '1b9f19211f844f04ba047bd98e206348.s1.eu.hivemq.cloud', # <<<****** Enter MQTT broker i.e. test.mosquitto.org
-  'mqtt_port' : '', # <<<******** Enter MQTT port i.e. 1883, 8883    (for HiveMQ cluster)
-  'mqtt_subscribe_topic' : '', # <<<******** enter name of MQTT to subscribe to i.e. tml/iot  
-  'mqtt_enabletls': '0', # set 1=TLS, 0=no TLSS  
+  'mqtt_port' : '8883', # <<<******** Enter MQTT port i.e. 1883, 8883    (for HiveMQ cluster)
+  'mqtt_subscribe_topic' : 'tml/cybersecurity', # <<<******** enter name of MQTT to subscribe to i.e. tml/iot  
+  'mqtt_enabletls': '1', # set 1=TLS, 0=no TLSS  
   'delay' : '7000', # << ******* 7000 millisecond maximum delay for VIPER to wait for Kafka to return confirmation message is received and written to topic
   'topicid' : '-999', # <<< ********* do not modify      
 }
